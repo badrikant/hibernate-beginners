70dee52f891376ace1249804aeadacc5f674f386
Badrikant Soni <badrikant.soni@inmobi.com>
2018-10-30 14:10:36 +0530

    first commit

---
+++ src/main/java/com/learn/jdbc/TestJdb.java
@@ -0,0 +1,29 @@
+package com.learn.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * @author badrikant.soni on Oct,2018
+ */
+public class TestJdb {
+
+    public static void main(String[] args) {
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+        String user = "hbstudent";
+        String pass = "hbstudent";
+
+        try {
+            System.out.println("Connecting to database:" + jdbcUrl);
+            Connection myConnection = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            System.out.println("Connection Successfully");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
