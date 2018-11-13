149b443dcbfef864fcadb3b93a06899ec77700f0
Badrikant Soni <badrikant.soni@inmobi.com>
2018-11-13 17:03:39 +0530

    added instructions

--- src/main/java/com/learn/hibernate/entity/InstructorDetail.java
+++ src/main/java/com/learn/hibernate/entity/InstructorDetail.java
@@ -22,6 +22,18 @@ import javax.persistence.Table;
 @Table(name = "instructor_detail")
 public class InstructorDetail {
 
+    // annotate the class as an entity and map to db table
+
+    // define the fields
+
+    // annotate the fields with db column names
+
+    // create constructors
+
+    // generate getter/setter methods
+
+    // generate toString() method
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
